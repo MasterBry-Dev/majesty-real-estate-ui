16f8dadd7e4f6b6d66377a644f558b9541638326
BryanSurio <bryansurio14@gmail.com>
2024-06-21 20:20:22 +0800

    added property inventory form route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 const Index = lazy(() => import('../pages/Index'));
 const PropertyInventory = lazy(() => import('../pages/PropertyInventory'));
+const PropertyForm = lazy(() => import('../pages/PropertyInventoryForm'));
 
 const routes = [
     // dashboard
@@ -9,6 +10,11 @@ const routes = [
         element: <PropertyInventory />,
         layout: 'default',
     },
+    {
+        path: '/PropertyForm',
+        element: <PropertyForm/>,
+        layout: 'default'
+    }
 
 ];
 
