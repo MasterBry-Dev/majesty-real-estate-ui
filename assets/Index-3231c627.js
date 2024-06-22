1a3a64061308a119398fcc952a0193fafd61b224
BryanSurio <bryansurio14@gmail.com>
2024-06-22 17:49:13 +0800

    Updates

---
+++ assets/Index-3231c627.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-05cc2244.js";const t=()=>r("div",{children:r("h1",{children:"starter page"})});export{t as default};
--- assets/Index-f70c3b0b.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ed5f544d.js";const t=()=>r("div",{children:r("h1",{children:"starter page"})});export{t as default};
