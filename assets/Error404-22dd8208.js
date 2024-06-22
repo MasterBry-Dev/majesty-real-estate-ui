c7ed9c8c66e228501892556cf3747bfa4d25468b
BryanSurio <bryansurio14@gmail.com>
2024-06-22 11:35:31 +0800

    Updates

---
+++ assets/Error404-22dd8208.js
@@ -0,0 +1,9 @@
+import{u as a,r as o,s as n,v as i,j as e,b as d,L as l}from"./index-36fa104c.js";const m=()=>{const t=a();o.useEffect(()=>{t(n("Error 404"))});const s=i(r=>r.themeConfig.theme==="dark"||r.themeConfig.isDarkMode);return e("div",{className:"relative flex min-h-screen items-center justify-center overflow-hidden",children:e("div",{className:`px-6 py-16 \r
+            text-center font-semibold \r
+            before:container \r
+            before:absolute \r
+            before:left-1/2 \r
+            before:-translate-x-1/2 \r
+            before:rounded-full \r
+            before:bg-[linear-gradient(180deg,#4361EE_0%,rgba(67,97,238,0)_50.73%)] \r
+            before:aspect-square before:opacity-10 md:py-20`,children:d("div",{className:"relative",children:[e("img",{src:s?"../dist/assets/images/error/404-dark.svg":"../dist/assets/images/error/404-light.svg",alt:"404",className:"mx-auto -mt-10 w-full max-w-xs object-cover md:-mt-14 md:max-w-xl"}),e("p",{className:"mt-5 text-xl text-dark dark:text-white",children:"The page you requested was not found!"}),e(l,{to:"/",className:"text-dark text-xl text-bold btn mx-auto !mt-7 w-max border-0 uppercase shadow-none",children:"Home"})]})})})};export{m as default};
---
+++ assets/Index-8189fb29.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-36fa104c.js";const t=()=>r("div",{children:r("h1",{children:"starter page"})});export{t as default};
