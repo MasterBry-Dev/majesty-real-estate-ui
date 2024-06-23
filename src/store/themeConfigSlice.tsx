3c8889efb100056139c823bd61440c8921be88cb
BryanSurio <113846848+MasterBry-Dev@users.noreply.github.com>
2024-06-23 18:00:33 +0800

    modify theme

--- src/store/themeConfigSlice.tsx
+++ src/store/themeConfigSlice.tsx
@@ -134,7 +134,7 @@ const themeConfigSlice = createSlice({
         },
 
         setPageTitle(state, { payload }) {
-            document.title = `${payload} | MY HOME TOWN`;
+            document.title = `${payload} | MAJESTY REAL ESTATE`;
         },
     },
 });
--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -5,8 +5,8 @@ const themeConfig = {
     layout: 'full', // full, boxed-layout
     rtlClass: 'ltr', // rtl, ltr
     animation: 'animate__slideInRight', // animate__fadeIn, animate__fadeInDown, animate__fadeInUp, animate__fadeInLeft, animate__fadeInRight, animate__slideInDown, animate__slideInLeft, animate__slideInRight, animate__zoomIn
-    navbar: 'navbar-floating', // navbar-sticky, navbar-floating, navbar-static
-    semidark: false
+    navbar: 'navbar-sticky', // navbar-sticky, navbar-floating, navbar-static
+    semidark: true
 };
 
 export default themeConfig;
