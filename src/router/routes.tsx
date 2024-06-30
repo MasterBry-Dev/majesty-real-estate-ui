deaaca5575df57898a9a00abb9a42d952a484c42
BryanSurio <113846848+MasterBry-Dev@users.noreply.github.com>
2024-06-30 18:12:40 +0800

    added new route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,19 @@
 import { lazy } from 'react';
-const Index = lazy(() => import('../pages/Index'));
+import { NavLink, Navigate } from 'react-router-dom';
+const Table = lazy(() => import('../pages/Mytable'));
 const PropertyInventory = lazy(() => import('../pages/PropertyInventory'));
 const PropertyForm = lazy(() => import('../pages/PropertyInventoryForm'));
 const Error404 = lazy(() => import('../pages/Error404'));
+const PropertyMatching = lazy(() => import('../pages/PropertyMatching'));
+
 
 const routes = [
     // dashboardz
+    {
+        path: '/',
+        element: <Navigate to='/property-inventory' />,
+        layout: 'default',
+    },
     {
         path: '/property-inventory',
         element: <PropertyInventory />,
@@ -20,7 +28,12 @@ const routes = [
         path: '*',
         element: <Error404/>,
         layout: 'blank'
-    }
+    },
+    {
+        path: '/property-matching',
+        element: <PropertyMatching />,
+        layout: 'default',
+    },
 
 ];
 
