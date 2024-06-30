13d1e1231eb17a328334884c9bbd0eddb7e578b8
BryanSurio <113846848+MasterBry-Dev@users.noreply.github.com>
2024-06-30 18:11:40 +0800

    added model and mockdata

---
+++ src/mockData/PropertyInventoryData.ts
@@ -0,0 +1,398 @@
+export const columns = [
+    { 
+        accessor: 'propertyCode', 
+        title: 'Property Code', 
+        sortable: true 
+    },
+    {
+        accessor: 'transactionType',
+        title: 'Transaction Type', 
+        sortable: true 
+    },
+    { 
+        accessor: 'propertyType', 
+        title: 'Property Type', 
+        sortable: true 
+    },
+    { 
+        accessor: 'HouseOrLotNumber', 
+        title: 'House/Lot #',
+        sortable: true 
+    },
+    { 
+        accessor: 'street', 
+        title: 'Street',
+        sortable: true 
+    },
+    { 
+        accessor: 'brgy_dist_municipality', 
+        title: 'Brgy/Dist/Municipality',
+        sortable: true 
+    },
+    { 
+        accessor: 'village_subdivision', 
+        title: 'Village/Subd.',
+        sortable: true 
+    },
+    { 
+        accessor: 'city', 
+        title: 'City',
+        sortable: true 
+    },
+    { 
+        accessor: 'lotArea', 
+        title: 'Lot Area',
+        sortable: true 
+    },
+    { 
+        accessor: 'floorArea', 
+        title: 'Floor Area',
+        sortable: true 
+    },
+    { 
+        accessor: 'price', 
+        title: 'Price',
+        sortable: true 
+    },
+    { 
+        accessor: 'description', 
+        title: 'Description',
+        sortable: true 
+    },
+    { 
+        accessor: 'classification', 
+        title: 'Classification',
+        sortable: true 
+    },
+    { 
+        accessor: 'dateSource', 
+        title: 'Date Source',
+        sortable: true 
+    },
+    { 
+        accessor: 'accountOfficer', 
+        title: 'Account Officer',
+        sortable: true 
+    },
+    { 
+        accessor: 'commision', 
+        title: 'Commision',
+        sortable: true 
+    },
+    { 
+        accessor: 'documents', 
+        title: 'Documents',
+        sortable: true 
+    },
+    { 
+        accessor: 'picture', 
+        title: 'Picture',
+        sortable: true 
+    },
+    { 
+        accessor: 'listingType', 
+        title: 'Listing Type',
+        sortable: true 
+    },
+    { 
+        accessor: 'contactPerson', 
+        title: 'Contact Person',
+        sortable: true 
+    },
+    { 
+        accessor: 'contactNo', 
+        title: 'Contact#',
+        sortable: true 
+    },
+    { 
+        accessor: 'company', 
+        title: 'Company',
+        sortable: true 
+    },
+    { 
+        accessor: 'description', 
+        title: 'Description',
+        sortable: true 
+    },
+    { 
+        accessor: 'status', 
+        title: 'Status',
+        sortable: true 
+    },
+    { 
+        accessor: 'lastUpdate', 
+        title: 'Last Update',
+        sortable: true 
+    },
+    { 
+        accessor: 'updatedBy', 
+        title: 'Updated By',
+        sortable: true 
+    },
+    { 
+        accessor: 'encodedBy', 
+        title: 'Encoded By',
+        sortable: true 
+    },
+    { 
+        accessor: 'contactNo', 
+        title: 'Contact#',
+        sortable: true 
+    },
+    { 
+        accessor: 'areaGroupSpecialist', 
+        title: 'Area Group Specialist',
+        sortable: true 
+    },
+    { 
+        accessor: 'remarks', 
+        title: 'Remarks',
+        sortable: true 
+    },
+    { 
+        accessor: 'approval', 
+        title: 'Approval',
+        sortable: true 
+    },
+    { 
+        accessor: 'landmarks', 
+        title: 'Landmarks',
+        sortable: true 
+    },
+    { 
+        accessor: 'structureAge', 
+        title: 'Structure Age',
+        sortable: true 
+    },
+    { 
+        accessor: 'idealFor', 
+        title: 'Ideal For',
+        sortable: true 
+    },
+    { 
+        accessor: 'frontage', 
+        title: 'Frontage',
+        sortable: true 
+    },
+    { 
+        accessor: 'flooding', 
+        title: 'Flooding',
+        sortable: true 
+    },
+    { 
+        accessor: 'orientation', 
+        title: 'Orientation',
+        sortable: true 
+    },
+    { 
+        accessor: 'sellerExpenses', 
+        title: 'Seller Expenses',
+        sortable: true 
+    },
+    { 
+        accessor: 'sourceBy', 
+        title: 'Source By',
+        sortable: true 
+    },
+    { 
+        accessor: 'sourceThru', 
+        title: 'Source Thru',
+        sortable: true 
+    },
+    { 
+        accessor: 'publishDate', 
+        title: 'Publish Date',
+        sortable: true 
+    },
+    { 
+        accessor: 'individual_corporation', 
+        title: 'Individual/Corporation',
+        sortable: true 
+    },
+    { 
+        accessor: 'url', 
+        title: 'URL',
+        sortable: true 
+    }
+]
+
+export const rowData = [
+    {
+        id: 1,
+        propertyCode: 'D-CAL1181',
+        transactionType: 'Sale',
+        propertyType: 'Vacant lot',
+        HouseOrLotNumber: 'Lot 31 & lot 34 blk 13',
+        street: 'St. Mary street',
+        brgy_dist_municipality: 'Camarin',
+        village_subdivision: 'Maligaya Park Subdivision',
+        city: 'Caloocan City',
+        lotArea: 603,
+        floorArea: 560,
+        price: '2,500,000.00',
+        description: 'Supposed to be vacant lot with informal settlers consists of 2 lots title with EJS',
+        classification: 'Residential',
+        dateSource: '13/04/2023',
+        accountOfficer: '',
+        commision: '',
+        documents: 'Tax Dec., lLot Plan, Title',
+        picture: '',
+        listingType: 'Owner',
+        contactPerson: 'Antonio De Guzman',
+        contactNo: '0939-571-9477',
+        company: '',
+        status: 'Available',
+        lastUpdate: '23/10/2023',
+        updatedBy: 'Cristal Villena',
+        encodedBy: 'Cristal Villena',
+        areaGroupSpecialist: '',
+        remarks: '*Cristal Villena from 1m net to 2.5m net *4/13/2023 - Cristal Villena all taxes and other expenses shoulder buyer',
+        approval: 'Approved',
+        landmarks: 'Beside lot 29 blk 13',
+        structureAge: '',
+        idealFor: '',
+        frontage: '',
+        flooding: '',
+        orientation: '',
+        sellerExpenses: 'Net Price',
+        sourceBy: 'Cristal Villena',
+        sourceThru: 'Walk-in',
+        publishDate: '13/04/2023',
+        individual_corporation: 'Individual',
+        url: '',
+
+    },
+    {
+        id: 2,
+        propertyCode: 'D-CAL1190',
+        transactionType: 'Sale',
+        propertyType: 'Vacant lot',
+        HouseOrLotNumber: 'Lot 31 & lot 34 blk 13',
+        street: 'St. Mary street',
+        brgy_dist_municipality: 'Camarin',
+        village_subdivision: 'Casa Park Subdivision',
+        city: 'Quezon City',
+        lotArea: 433,
+        floorArea: 280,
+        price: '4,000,000.00',
+        description: '',
+        classification: 'Residential',
+        dateSource: '30/04/2023',
+        accountOfficer: '',
+        commision: '',
+        documents: 'Lot Plan, Title',
+        picture: '',
+        listingType: 'Owner',
+        contactPerson: 'John Guevarra',
+        contactNo: '0929-351-9471',
+        company: '',
+        status: 'Available',
+        lastUpdate: '23/12/2023',
+        updatedBy: 'John mema',
+        encodedBy: 'John mema',
+        areaGroupSpecialist: '',
+        remarks: '*sad',
+        approval: 'Approved',
+        landmarks: 'Near jollibee',
+        structureAge: '',
+        idealFor: '',
+        frontage: '',
+        flooding: '',
+        orientation: '',
+        sellerExpenses: '',
+        sourceBy: 'ohn mema',
+        sourceThru: 'Walk-in',
+        publishDate: '20/02/2023',
+        individual_corporation: 'Individual',
+        url: '',
+    },
+    {
+        id: 1,
+        propertyCode: 'D-CAL1181',
+        transactionType: 'Sale',
+        propertyType: 'Vacant lot',
+        HouseOrLotNumber: 'Lot 31 & lot 34 blk 13',
+        street: 'St. Mary street',
+        brgy_dist_municipality: 'Camarin',
+        village_subdivision: 'Maligaya Park Subdivision',
+        city: 'Caloocan City',
+        lotArea: 603,
+        floorArea: 560,
+        price: '2,500,000.00',
+        description: 'Supposed to be vacant lot with informal settlers consists of 2 lots title with EJS',
+        classification: 'Residential',
+        dateSource: '13/04/2023',
+        accountOfficer: '',
+        commision: '',
+        documents: 'Tax Dec., lLot Plan, Title',
+        picture: '',
+        listingType: 'Owner',
+        contactPerson: 'Antonio De Guzman',
+        contactNo: '0939-571-9477',
+        company: '',
+        status: 'Available',
+        lastUpdate: '23/10/2023',
+        updatedBy: 'Cristal Villena',
+        encodedBy: 'Cristal Villena',
+        areaGroupSpecialist: '',
+        remarks: '*Cristal Villena from 1m net to 2.5m net *4/13/2023 - Cristal Villena all taxes and other expenses shoulder buyer',
+        approval: 'Approved',
+        landmarks: 'Beside lot 29 blk 13',
+        structureAge: '',
+        idealFor: '',
+        frontage: '',
+        flooding: '',
+        orientation: '',
+        sellerExpenses: 'Net Price',
+        sourceBy: 'Cristal Villena',
+        sourceThru: 'Walk-in',
+        publishDate: '13/04/2023',
+        individual_corporation: 'Individual',
+        url: '',
+
+    },
+    {
+        id: 2,
+        propertyCode: 'D-CAL1190',
+        transactionType: 'Sale',
+        propertyType: 'Vacant lot',
+        HouseOrLotNumber: 'Lot 31 & lot 34 blk 13',
+        street: 'St. Mary street',
+        brgy_dist_municipality: 'Camarin',
+        village_subdivision: 'Casa Park Subdivision',
+        city: 'Quezon City',
+        lotArea: 433,
+        floorArea: 280,
+        price: '4,000,000.00',
+        description: '',
+        classification: 'Residential',
+        dateSource: '30/04/2023',
+        accountOfficer: '',
+        commision: '',
+        documents: 'Lot Plan, Title',
+        picture: '',
+        listingType: 'Owner',
+        contactPerson: 'John Guevarra',
+        contactNo: '0929-351-9471',
+        company: '',
+        status: 'Available',
+        lastUpdate: '23/12/2023',
+        updatedBy: 'John mema',
+        encodedBy: 'John mema',
+        areaGroupSpecialist: '',
+        remarks: '*sad',
+        approval: 'Approved',
+        landmarks: 'Near jollibee',
+        structureAge: '',
+        idealFor: '',
+        frontage: '',
+        flooding: '',
+        orientation: '',
+        sellerExpenses: '',
+        sourceBy: 'ohn mema',
+        sourceThru: 'Walk-in',
+        publishDate: '20/02/2023',
+        individual_corporation: 'Individual',
+        url: '',
+    }
+]
